35a7381b57605eb7e90d55dc47c3bfeb63a93b27
Himadri Behera <himadribehera321@gmail.com>
2025-07-26 17:14:33 +0530

    Fixed with ViewDrop

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { store } from "./redux/store.js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { Provider } from "react-redux";
-import { Toaster } from "react-hot-toast";
+import { store, persistor } from "./redux/store"; // make sure the path is correct
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-      <Toaster />
-    </Provider>
-  </StrictMode>
+    </PersistGate>
+  </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import dropReducer from './dropSlice'
+// store.js or redux/store.js
+
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import dropReducer from "./dropSlice"; // your existing reducer
+import { combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  drop: dropReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {drop: dropReducer,},
-})
+  reducer: persistedReducer,
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
